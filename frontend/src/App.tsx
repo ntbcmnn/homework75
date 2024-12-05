c9dd610885861795c8d251778f5799eafc51d8f4
Natalya Botsman <botsmannatashaa@gmail.com>
2024-12-05 18:21:28 +0600

    Displayed toolbar and form components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
 import './App.css';
+import AppToolbar from './Components/UI/AppToolbar/AppToolbar.tsx';
+import CipherForm from './containers/CipherForm/CipherForm.tsx';
+import { Container, CssBaseline } from '@mui/material';
 
 const App = () => {
-  return <></>;
+  return <>
+    <CssBaseline/>
+    <header>
+      <AppToolbar/>
+    </header>
+
+    <main>
+      <Container maxWidth="xl">
+        <CipherForm/>
+      </Container>
+    </main>
+  </>;
 };
 
 export default App;
